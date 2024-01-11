ee3491c6940a64df7ecc77afdaf9893f6c15a7fa
Anass-Dr <anasdrissi77@gmail.com>
2024-01-11 11:02:19 +0100

    Logout route added

--- public/index.php
+++ public/index.php
@@ -14,16 +14,25 @@ $method = strtolower($_SERVER["REQUEST_METHOD"]);
 # Initialize new Router Object :
 $router = new App\Core\Router();
 
-# Add Routing :
-$router->get('/', App\Controllers\HomeController::class, 'index');
+# ------------------------------------ #
+# --------- Add Routing : ------------ #
+# ------------------------------------ #
+
+# -> Authentification :
 $router->get('/login', App\Controllers\Authentification::class, 'loginHTML');
 $router->post('/login', App\Controllers\Authentification::class, 'login');
 $router->get('/register', App\Controllers\Authentification::class, 'registerHTML');
 $router->post('/register', App\Controllers\Authentification::class, 'register');
-$router->get('/wiki', App\Controllers\WikiController::class, 'index');
+$router->get('/logout', App\Controllers\Authentification::class, 'logout');
+
+# -> Home Page :
+$router->get('/', App\Controllers\HomeController::class, 'index');
 $router->get('/wikis', App\Controllers\WikiController::class, 'getAll');
 $router->get('/lastWikis', App\Controllers\WikiController::class, 'getLatest');
 $router->get('/tags', App\Controllers\TagController::class, 'getAll');
+
+#
+$router->get('/wiki', App\Controllers\WikiController::class, 'index');
 
 
 #
