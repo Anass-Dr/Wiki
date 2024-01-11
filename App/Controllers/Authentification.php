1688a47380641e259a7943907e78bad397568d5c
Anass-Dr <anasdrissi77@gmail.com>
2024-01-11 10:59:03 +0100

    Login functionalities updated and logout added

--- App/Controllers/Authentification.php
+++ App/Controllers/Authentification.php
@@ -12,8 +12,27 @@ class Authentification extends Controller
     public static function login()
     {
         self::setHeaderJson();
-        $data = json_encode(file_get_contents("php://input"), true);
-        echo json_encode($data);
+        $data = json_decode(file_get_contents("php://input"), true);
+        extract($data);
+
+        $msg = '';
+
+        $obj = new \App\Repository\UserRepository();
+        $items = $obj->fetchOne([["email", $email]]);
+
+        if ($items) {
+            if (password_verify($password, $items[0]->password)) {
+                $_SESSION["user_id"] = $items[0]->id;
+                $_SESSION["user_role"] = $items[0]->role;
+
+                $msg = "ok";
+            } else {
+                $msg = "Password Incorrect";
+            }
+        } else {
+            $msg = "User Not Found";
+        }
+        echo json_encode(["msg" => $msg]);
     }
 
     public static function registerHTML()
@@ -30,5 +49,12 @@ class Authentification extends Controller
         $obj = new \App\Repository\UserRepository();
         $result = $obj->add([NULL, $username, $email, $password, "Author"]);
         echo json_encode(["msg" => $result]);
+    }
+
+    public static function logout()
+    {
+        session_unset();
+        session_destroy();
+        header("location:/");
     }
 }
